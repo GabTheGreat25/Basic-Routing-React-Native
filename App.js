035c8c925a3e1366cccee27aa18d6169fc3ca472
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2023-09-23 14:44:04 +0800

    Remove the header

--- App.js
+++ App.js
@@ -1,19 +1,31 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
-import slide1 from "./components/slide1";
-import slide2 from "./components/slide2";
-import slide3 from "./components/slide3";
+import Slide1 from "./components/slide1";
+import Slide2 from "./components/slide2";
+import Slide3 from "./components/slide3";
 
 const Stack = createStackNavigator();
 
-export default function () {
+export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="slide1">
-        <Stack.Screen name="slide1" component={slide1} />
-        <Stack.Screen name="slide2" component={slide2} />
-        <Stack.Screen name="slide3" component={slide3} />
+        <Stack.Screen
+          name="slide1"
+          component={Slide1}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="slide2"
+          component={Slide2}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="slide3"
+          component={Slide3}
+          options={{ headerShown: false }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
